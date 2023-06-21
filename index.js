87eb3538fb155e30d1346444bc518234b2b30b2e
Dennis Lustre <dlustre@uci.edu>
2023-06-20 22:57:43 -0700

    implement counter

--- index.js
+++ index.js
@@ -1,12 +1,87 @@
-document.getElementById("targetBtn").onclick = function () {
-    let randomX = Math.floor(Math.random() * (100 - (-100) + 1)) + -100;
-    let randomY = Math.floor(Math.random() * (100 - (-100) + 1)) + -100;
+// multiplayer
+// play with a friend option
+// player with most clickcount wins
 
-    let button = document.getElementById('targetBtn');
+let clickCount = 0;
 
-    button.style.backgroundColor = 'red';
+let button = document.getElementById('targetBtn');
+
+// Load click count
+if (document.cookie.includes('clickCount=')) {
+    clickCount = parseInt(document.cookie.split('clickCount=')[1]);
+}
+
+
+
+document.getElementById('clickCount').textContent = clickCount;
+
+// button.addEventListener('mousedown', function () {
+//     button.style.border = '100px';
+//     button.style.height = '100px';
+
+// });
+
+// button.addEventListener('mousedown', function () {
+//     button.style.width = '50px';
+//     button.style.height = '50px';
+// });
+
+// Add event listener for click
+button.addEventListener('click', function (event) {
+    // Create a new overlay element
+    let overlay = document.createElement('div');
+    overlay.classList.add('overlay');
+
+    // Set the position of the overlay to the previous position of the button
+    let buttonRect = button.getBoundingClientRect();
+    overlay.style.left = buttonRect.left + 'px';
+    overlay.style.top = buttonRect.top + 'px';
+
+    // Append the overlay to the button's parent element
+    button.parentNode.appendChild(overlay);
+
+    // Add the "clicked" class to the button
+    button.classList.add('clicked');
+
+    // Move the button to a new position
+    // ...
+    // Your code to move the button goes here
+
+    // Remove the "clicked" class and the overlay element after the fade-out animation ends
+    overlay.addEventListener('animationend', function () {
+        button.classList.remove('clicked');
+        overlay.remove();
+    });
+});
+
+button.onclick = function () {
+    clickCount++;
+
+    document.getElementById('clickCount').textContent = clickCount;
+
+    let randomX = Math.floor(Math.random() * (300 - (-300) + 1)) + -300;
+    let randomY = Math.floor(Math.random() * (300 - (-300) + 1)) + -300;
+
+    let counter = document.getElementById('clickCount');
+
+    let prevButtonColor = button.style.backgroundImage;
+    button.style.backgroundImage = 'none';
+    button.style.transition = 'background-image 0.5s'
+    counter.style.color = 'white';
+    counter.style.transition = 'color 0.5s';
+
+    button.classList.add('clicked');
+
+
+
     setTimeout(function () {
-        button.style.backgroundColor = '#4CAF50';
-    }, 50);
+        button.classList.remove('clicked');
+        button.style.backgroundImage = '';
+        counter.style.color = '';
+    }, 100);
     button.style.transform = 'translate(' + randomX + 'px, ' + randomY + 'px';
 }
+
+window.addEventListener('beforeunload', function () {
+    this.document.cookie = 'clickCount=' + clickCount;
+});
